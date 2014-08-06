a0dedd897431e4618cbe898d488df5cb2d2023b9
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-06 13:48:48 +0100

    Work on login

--- OAMonitor/grails-app/conf/BootStrap.groovy
+++ OAMonitor/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,52 @@
+import uk.ac.jisc.oamonitor.*
+
 class BootStrap {
 
-    def init = { servletContext ->
+  def grailsApplication
+
+  def init = { servletContext ->
+
+    if ( grailsApplication.config.localauth ) {
+      log.debug("localauth is set.. ensure user accounts present (From local config file) ${grailsApplication.config.sysusers}");
+
+      grailsApplication.config.sysusers.each { su ->
+        log.debug("test ${su.name} ${su.pass} ${su.display} ${su.roles}");
+        def user = User.findByUsername(su.name)
+        if ( user ) {
+          if ( user.password != su.pass ) {
+            log.debug("Hard change of user password from config ${user.password} -> ${su.pass}");
+            user.password = su.pass;
+            user.save(failOnError: true)
+          }
+          else {
+            log.debug("${su.name} present and correct");
+          }
+        }
+        else {
+          log.debug("Create user...");
+          user = new User(
+                        username: su.name,
+                        password: su.pass,
+                        display: su.display,
+                        email: su.email,
+                        enabled: true).save(failOnError: true)
+        }
+
+        log.debug("Add roles for ${su.name}");
+        su.roles.each { r ->
+          def role = Role.findByAuthority(r)
+          if ( ! ( user.authorities.contains(role) ) ) {
+            log.debug("  -> adding role ${role}");
+            UserRole.create user, role
+          }
+          else {
+            log.debug("  -> ${role} already present");
+          }
+        }
+      }
     }
-    def destroy = {
-    }
+  }
+
+  def destroy = {
+  }
 }
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 
 
-www.journaltocs.ac.uk
+
+
+# Sample ~/.grails/OAMonitor-config.groovy file for dev box
+
+localauth=true
+sysusers = [
+  [
+    name:'admin',
+    pass:'localadmpass',
+    display:'Admin',
+    email:'admin@localhost',
+    roles: [ 'ROLE_USER', 'ROLE_ADMIN']
+  ]
+]
