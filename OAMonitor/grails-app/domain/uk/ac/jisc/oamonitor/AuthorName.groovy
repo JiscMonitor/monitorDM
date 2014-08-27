70a8a3a271e9418c877a82e746e672b37ca341eb
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-27 11:25:20 +0100

    Added some suggestions to change AuthorName to ArticleAuthor and changed the relationship with person

--- OAMonitor/grails-app/domain/uk/ac/jisc/oamonitor/AuthorName.groovy
+++ OAMonitor/grails-app/domain/uk/ac/jisc/oamonitor/AuthorName.groovy
@@ -1,12 +1,24 @@
 package uk.ac.jisc.oamonitor
 
+
+/* todo: Rename to ArticleAuthor */
 class AuthorName {
 
+
+    /* The article this ArticleAuthor relates to */
+    Article theArticle
+
+    /* The name as it appeared */
     String fullname
 
-    static hasMany = [authorMatches:Person]
+    /* Any matched person */
+    Person matchedPerson
+
+    /* todo : add Boolean isCorrespondingAuthor - to identify primary author */
 
     static constraints = {
-        fullname unique: true, nullable: false, blank: false
+        fullname unique: false, nullable: false, blank: false
+        matchedPerson nullable: true
+        /* todo isCorrespondingAuthor mapping */
     }
 }
